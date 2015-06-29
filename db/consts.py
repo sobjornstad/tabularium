4fd6bb615dcb143f0ee276a451cb8e23e746ef98
Soren I. Bjornstad <soren.bjornstad@gmail.com>
2015-06-29 11:56:23 -0500

    add dictionaries for entry/occurrence types
    Not using them yet, but we might as well add them in.

---
+++ db/consts.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015 Soren Bjornstad <contact@sorenbjornstad.com>
+
+refTypes = {'num': 0, 'range': 1, 'redir': 2}
+
+entryTypes = {'unclassified': 0,
+              'ord': 1,
+              'name': 2,
+              'place': 3,
+              'quote': 4,
+              'title': 5}
