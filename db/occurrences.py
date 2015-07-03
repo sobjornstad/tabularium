66ea49992840a91d7d6f449f941b4f231a2de5d6
Soren I. Bjornstad <soren.bjornstad@gmail.com>
2015-07-03 15:49:01 -0500

    implement nearby retrieval in backend
    The database now needs to be upgraded in some way to include reftypes so
    that we can try actually using it, plus of course the UI code needs to
    be written.

--- db/occurrences.py
+++ db/occurrences.py
@@ -84,6 +84,56 @@ class Occurrence(object):
                 self._ref, self._type, self._de, self._da, self._oid))
         d.checkAutosave()
 
+    def getNearby(self, nearRange=1):
+        """
+        Find all occurrences that are in the same notebook/source and within
+        /nearRange/ pages/indices of it. Eventually /nearRange/ should be part
+        of the source options; for now this will ordinarily use the default
+        value of 1. Returns a list of Entry objects.
+
+        If the current occurrence is a redirect and thus has no logical result
+        for this operation, or if the range or other data is otherwise invalid
+        for this occurrence, or in the very unusual case that there are simply
+        no results, return None. #(would we rather several return types?)
+
+        Note that nearby is capable of finding things that are nearby ranges,
+        but is not currently always capable of finding ranges themselves in
+        nearby queries. (SQL BETWEEN does successfully find the entry when one
+        of the top or bottom range numbers is actually in the string.)
+        """
+
+        if not (self._type == 1 or self._type == 0):
+            return None
+
+        page = self._ref
+        if self._type == 1:
+            retval = parseRange(page)
+            if retval is None:
+                return None
+            else:
+                bottom, top = retval
+            pageStart = bottom - nearRange
+            pageEnd = top + nearRange
+        else:
+            try:
+                pageStart = int(page) - 1
+                pageEnd = int(page) + 1
+            except ValueError:
+                return None
+
+        q = '''SELECT oid FROM occurrences
+               WHERE nid = ? AND (type = 0 OR type = 1)
+                   AND ref BETWEEN ? AND ?'''
+        d.cursor.execute(q, (self._notebook.getNid(), pageStart, pageEnd))
+        occs = [Occurrence(i[0]) for i in d.cursor.fetchall()]
+
+        # fetch list of unique entries nearby
+        entries = [i.getEntry() for i in occs]
+        entries.sort(key=lambda i: i._sk)
+
+        return entries
+
+
 def fetchForEntry(entry):
     """
     Return a list of all Occurrences for a given Entry.
@@ -92,3 +142,18 @@ def fetchForEntry(entry):
     eid = entry.getEid()
     d.cursor.execute('SELECT oid FROM occurrences WHERE eid=?', (eid,))
     return [Occurrence(i[0]) for i in d.cursor.fetchall()]
+
+def parseRange(val):
+    """
+    Return a tuple of bottom, top integers for a range (a string consisting of
+    two ints separated by a hyphen). Return None if the value was not a range
+    in this format.
+    """
+
+    try:
+        bottom, top = val.split('-')
+        bottom, top = int(bottom), int(top)
+    except ValueError:
+        return None
+
+    return (bottom, top)
--- tests/test_occurrences.py
+++ tests/test_occurrences.py
@@ -36,3 +36,43 @@ class OccTests(utils.DbTestCase):
         assert len(occs) == 1
         assert occs[0] == o1
 
+    def testNearby(self):
+        ### Test proper returns and lack thereof.
+        n1 = Notebook('CB', 2, '2015-01-01', '2015-02-01')
+        n2 = Notebook('TB', 3, '2015-02-01', '2015-03-01')
+
+        e1 = Entry.makeNew("Kathariana")
+        e2 = Entry.makeNew("Melgreth, Maudia")
+        e3 = Entry.makeNew("Vaunder, Salila")
+        e4 = Entry.makeNew("Maud")
+        e5 = Entry.makeNew("Elibemereth")
+        e6 = Entry.makeNew("Whilla, Lianja")
+        e7 = Entry.makeNew("Kaitlyn Complex")
+
+        # we want this set to appear on query of e1
+        o1 = Occurrence.makeNew(e1, n1, '25', 0)
+        o2 = Occurrence.makeNew(e2, n1, '26', 0)
+        o3 = Occurrence.makeNew(e3, n1, '24', 0)
+        o4 = Occurrence.makeNew(e4, n1, '25-28', 0)
+
+        # but these should not, for various reasons
+        o5 = Occurrence.makeNew(e5, n1, '29', 0) # too far away
+        o6 = Occurrence.makeNew(e6, n2, '25', 0) # wrong notebook
+        o7 = Occurrence.makeNew(e7, n1, '25', 2) # wrong reftype
+
+        r = o1.getNearby()
+        assert len(r) == 4, len(r)
+        assert e1 in r, 'own occ not in nearby'
+        assert e2 in r, 'adjacent occ not in nearby'
+        assert e3 in r, 'adjacent occ not in nearby'
+        assert e4 in r, 'range on boundary not in nearby'
+        assert e5 not in r, 'overly distant occ in nearby'
+        assert e6 not in r, 'wrong notebook occ in nearby'
+        assert e7 not in r, 'redirect reftype in nearby'
+
+        # sort order (alphabetical)
+        order = [o1, o4, o2, o3]
+        entryOrder = [i.getEntry() for i in order]
+        assert r == entryOrder
+
+        ### test none returns for invalid entries?
