2dc7d518eb4b9e3113e0d685c681491284e6f8bd
Soren I. Bjornstad <contact@sorenbjornstad.com>
2022-06-28 06:27:38 -0500

    set up doctests

--- docs/addAnchors.py
+++ docs/addAnchors.py
@@ -10,21 +10,22 @@ def replaceFunc(txt):
     txt = txt.replace(',', '')
     return txt
 
-if len(sys.argv) <= 2:
-    print "Usage: addAnchors input.md output.md"
-    sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) <= 2:
+        print("Usage: addAnchors input.md output.md")
+        sys.exit(1)
 
-readname = sys.argv[1]
-writename = sys.argv[2]
-with open(readname) as f:
-    markdownLines = f.readlines()
+    readname = sys.argv[1]
+    writename = sys.argv[2]
+    with open(readname) as f:
+        markdownLines = f.readlines()
 
-with open(writename, 'w') as f:
-    for line in markdownLines:
-        if line.startswith('#'):
-            match = re.match(r'^(\#+) (.*)', line)
-            hashes, text = match.group(1), match.group(2)
-            modtext = replaceFunc(text.lower())
-            f.write('<a id="%s"></a>\n%s %s' % (modtext, hashes, text))
-        else:
-            f.write(line)
+    with open(writename, 'w') as f:
+        for line in markdownLines:
+            if line.startswith('#'):
+                match = re.match(r'^(\#+) (.*)', line)
+                hashes, text = match.group(1), match.group(2)
+                modtext = replaceFunc(text.lower())
+                f.write('<a id="%s"></a>\n%s %s' % (modtext, hashes, text))
+            else:
+                f.write(line)
